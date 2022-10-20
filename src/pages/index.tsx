b0d5413ef46731a085dfaef9c2c4286b97eb9ef4
xuniorss <fortunatojunior2020@gmail.com>
2022-10-20 13:09:29 -0300

    form-login-nao-terminado

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,31 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/styles.module.scss'
 
 export default function Home() {
     return (
-        <div className={styles.container}>
+        <>
+            <Head>
+                <title>Account - Fortnite.GG</title>
+            </Head>
+            <div className={styles.container}>
+                <div className={styles.login}>                    
+                    <form>
+                        <input type="text" placeholder='Email' />
+                        <input type="text" placeholder='Password' />
 
-        </div>
+                        <button type="submit">Sign In</button>
+
+                        <Link href={'/'}>
+                            <a>Create account</a>
+                        </Link>
+                    </form>
+                    <p>This account is for this site only Do NOT enter your Epic Games login credentials</p>
+                </div>
+            </div>
+        </>
     )
 }
+
+//https://dash.fortnite-api.com/
