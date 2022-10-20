63baeb06949e6aa08d17bf7b72017c73fda97521
xuniorss <fortunatojunior2020@gmail.com>
2022-10-20 11:02:36 -0300

    initial-config

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+    return (
+        <div className={styles.container}>
+
+        </div>
+    )
+}
