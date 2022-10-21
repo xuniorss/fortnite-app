c2ec04ce140e15c6d954cc4c59cb1a1034d2ed87
xuniorss <fortunatojunior2020@gmail.com>
2022-10-21 16:34:52 -0300

    insert-logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,14 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import styles from './styles.module.scss'
+import logo from '../../../public/assets/logo.png'
 
 export function Header() {
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
-                <Link href={'#'}>
-                    <a>Fortnite.gg</a>
+                <Link href={'/shop'}>
+                    <Image src={logo} alt='logo' width={300} height={50} />
                 </Link>
                 <nav>
                     <Link href={'/shop'}>
